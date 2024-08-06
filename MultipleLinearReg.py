5b2ffea10d6ab2e11ca2c931dd3f823065e3705c
DotCsanova <albertolopezcasanova@icloud.com>
2024-08-06 18:00:29 +0200

    Segundo commit. Inclusión librerías python

---
+++ MultipleLinearReg.py
@@ -0,0 +1,7 @@
+#En primer lugar, al igual que con regresión lineal simple y con el resto, necesitamos
+#importar las librerías de las que vayamos hacer uso
+
+import matplotlib.pyplot as plt #Para representación de gráficas
+import numpy as np #Para realizar cálculos numéricos eficientes con datos (nos da los ndarray por ej.)
+import pandas as pd #Ofrece el objeto DataFrame, que nos permite trabajar con datos tabulares enriquecidos con etiquetas en filas y columnas.
+import pylab as pl #Junta en un mismo espacio de nombres funciones de MatplotLib y Numpy, asemejándose a Matlab
